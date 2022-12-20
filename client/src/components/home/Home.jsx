d5c56cf55df0e6faa2bd6ded11f468d84752aec8
Afolabi Opeyemi <yemijoshua81@gmail.com>
2022-12-20 12:39:24 +0100

    adding a section to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import welcome from "../../images/welcome.jpg";
 import Navigation from "./Navigation";
+import protection from "../../images/download.jfif";
 import "./home.css";
 
 export default function Home() {
-  
   return (
     <div>
       {/* --------------------------------------- Navigation ---------------------------------------- */}
@@ -19,7 +19,7 @@ export default function Home() {
             <p>
               Discovercreditunion <sup>&copy;</sup>
             </p>
-          </div> 
+          </div>
           <div className="welcome-img">
             <img src={welcome} alt="welcome" />
           </div>
@@ -62,6 +62,78 @@ export default function Home() {
         </div>
         <hr />
       </section>
+
+      {/* --------------------------------------- Services section ---------------------------------------- */}
+
+      {/* --------------------------------------- D-Services section ---------------------------------------- */}
+      <section className="sub-services">
+        <div className="sub-items">
+          <div className="sub-item">
+            <div className="sub-bg"></div>
+            <div className="sub-text">
+              <h5>Online Privacy Protection for free</h5>
+              <p>
+                We help remove your personal data from ten popular people-search
+                websites. Activate by mobile app for free.*
+              </p>
+              <a href="#">Learn more</a>
+            </div>
+          </div>
+          <div className="sub-item">
+            <div className="sub-bg"></div>
+            <div className="sub-text">
+              <h5>5% cash back when you shop</h5>
+              <p>
+                Earn 5% Cashback Bonus® at Amazon.com and Digital Wallets,
+                now–December, on up to $1,500 in purchases, when you activate.
+              </p>
+              <a href="#">Check now</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="sub-services">
+        <div className="sub-items">
+          <div className="sub-item sub-item2 rad-1">
+            <div className="sub-bg"></div>
+            <div className="sub-text">
+              <h5>Free benefits that just make sense</h5>
+              <p>
+                Card and explore your benefits. Security, financial insights,
+                customer service and more.
+              </p>
+              <a href="#">Explore Benefits</a>
+            </div>
+          </div>
+          <div className="sub-item sub-item2">
+            <div className="sub-bg"></div>
+            <div className="sub-text">
+              <h5>Feeling safer at checkout</h5>
+              <p>
+                Tap and pay with a contactless Discover® credit card to help you
+                avoid touching the card reader.
+              </p>
+              <a href="#">Update Card today</a>
+            </div>
+          </div>
+          <div className="sub-item sub-item2 rad-2">
+            <div className="sub-bg"></div>
+            <div className="sub-text">
+              <h5>Easy credit card preapproval check</h5>
+              <p>
+                Find out if you're preapproved for a Discover® card with no
+                impact to your credit to check.
+              </p>
+              <a href="#">Check if you're pre approved</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* --------------------------------------- !D-Services section ---------------------------------------- */}
+
+      {/* --------------------------------------- D-Services section ---------------------------------------- */}
+      {/* --------------------------------------- D-Services section ---------------------------------------- */}
 
     </div>
   );
